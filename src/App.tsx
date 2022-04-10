1aa6dfc94fc6b518b1c4c1e1786d19d22d252401
deedeeh <dinahafez.8@gmail.com>
2022-04-10 12:10:59 +0100

    Created a form to add cats and updated cats array

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
 import './App.css';
 import Cat from './data/cat';
 import Dog from './data/dog';
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
 import Card from './components/card';
 import catData from './data/catData';
 import dogData from './data/dog-data';
+import { v4 as uuidv4 } from "uuid";
+
+interface Card {
+	name: string;
+	species: string;
+	favFoods: Array<string>;
+	birthYear: number;
+}
 
 function App() {
 	// JavaScript code can be inserted here!
 	const [ cats, setCats ] = useState<Array<Cat>>(catData);
 	const [ dogs, setDogs ] = useState<Array<Dog>>(dogData);
+	const [ name, setName ] = useState<string>('');
+	const [ species, setSpecies ] = useState<string>('');
+	const [ favFoods, setFavFoods ] = useState<Array<string>>([]);
+	const [ favFood, setFavFood ] = useState<string>('');
+	const [ birthYear, setBirthYear ] = useState<number>(2020);
 
 	const catCount = cats.length;
 	const dogCount = dogs.length;
+
+	const resetForm = () => {
+		setName('');
+		setSpecies('');
+		setFavFoods([]);
+		setFavFood('');
+	}
+	
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const id = uuidv4()
+		const card = { id, name, species, favFoods, birthYear }
+		setCats([...cats, card])
+		resetForm();
+	}
 	
 	return (
 		<>
@@ -48,6 +76,56 @@ function App() {
 						/>)
 				}</div>
 			</main>
+			<form onSubmit={handleSubmit}>
+				<fieldset>
+					<label>
+						<p>Name:</p>
+						<input 
+							type='text' 
+							name='name' 
+							value={name}
+							onChange={(e => setName(e.target.value))}
+						/>
+					</label>
+				</fieldset>
+				<fieldset>
+					<label>
+						<p>Species:</p>
+						<input 
+							type='text' 
+							name='species' 
+							value={species}
+							onChange={(e => setSpecies(e.target.value))}
+						/>
+					</label>
+				</fieldset>
+				<fieldset>
+					<label>
+						<p>Favourite Food(s):</p>
+						<input 
+							type='text' 
+							name='favFoods' 
+							value={favFood}
+							onChange={(e => {
+								setFavFood(e.target.value)
+								setFavFoods([e.target.value])
+							})}
+						/>
+					</label>
+				</fieldset>
+				<fieldset>
+					<label>
+						<p>Birth Year:</p>
+						<input 
+							type='number' 
+							name='birthYear' 
+							value={birthYear}
+							onChange={(e => setBirthYear(Number(e.target.value)))}
+						/>
+					</label>
+				</fieldset>
+				<input type="submit" value="Submit" />
+			</form>
 			<Footer />
 		</>
 	);
